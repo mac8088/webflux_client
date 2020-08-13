d610d5339bdf70c7d9a95410b71ce81c7cfb6420
MaChun <chun.ma@atos.net>
2020-08-13 22:51:15 +0800

    init from start.spring.io

---
+++ src/main/java/com/imooc/webfluxclient/WebfluxclientApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.webfluxclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxclientApplication.class, args);
+	}
+
+}
