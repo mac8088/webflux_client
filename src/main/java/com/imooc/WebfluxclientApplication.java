f68a069d69a243b159d5186b85a30eb80cab7252
MaChun <chun.ma@atos.net>
2020-08-13 22:53:18 +0800

    changed port: 8081

--- src/main/java/com/imooc/WebfluxclientApplication.java
+++ src/main/java/com/imooc/WebfluxclientApplication.java
@@ -1,4 +1,4 @@
-package com.imooc.webfluxclient;
+package com.imooc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/imooc/WebfluxclientApplicationTests.java
+++ src/test/java/com/imooc/WebfluxclientApplicationTests.java
@@ -1,4 +1,4 @@
-package com.imooc.webfluxclient;
+package com.imooc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
