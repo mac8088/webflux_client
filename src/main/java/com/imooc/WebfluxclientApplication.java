9276e4f8857acc1392d39f3d65aeb400a6fe88b5
MaChun <chun.ma@atos.net>
2020-08-16 15:37:10 +0800

    测试联调

--- src/main/java/com/imooc/WebfluxclientApplication.java
+++ src/main/java/com/imooc/WebfluxclientApplication.java
@@ -1,13 +1,49 @@
 package com.imooc;
 
+import org.springframework.beans.factory.FactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.imooc.interfaces.ProxyCreator;
+import com.imooc.proxys.JDKProxyCreator;
 
 @SpringBootApplication
 public class WebfluxclientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebfluxclientApplication.class, args);
+	}
+
+	/**
+	 * 创建jdk代理工具类
+	 * 
+	 * @return
+	 */
+	@Bean
+	ProxyCreator jdkProxyCreator() {
+		return new JDKProxyCreator();
+	}
+
+	/**
+	 * 返回代理对象
+	 * 
+	 * @return
+	 */
+	@Bean
+	FactoryBean<IUserApi> userApi(ProxyCreator proxyCreator) {
+		return new FactoryBean<IUserApi>() {
+
+			@Override
+			public Class<?> getObjectType() {
+				return IUserApi.class;
+			}
+
+			@Override
+			public IUserApi getObject() throws Exception {
+				return (IUserApi) proxyCreator.createProxy(this.getObjectType());
+			}
+		};
 	}
 
 }
